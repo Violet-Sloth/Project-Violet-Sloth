94ee6d6a8630254efa2a045252d2c4c84a9c0b6d
Mike McGonagle <mcmcgonagle815@gmail.com>
2017-10-10 10:08:30 -0400

    Fixed type and indentation issues
    Fixed the typo and indentation issues CJ spotted.

--- dail-e.py
+++ dail-e.py
@@ -7,7 +7,7 @@ wotdDB = boto3.client('dynamodb')
 
 #Lambda Handler function
 def lambda_handler(event, context):
-	#Scrap WOTD XML Feed for Today's word
+	#Scrape WOTD XML Feed for Today's word
 	response  = requests.get("https://wotd.transparent.com/rss/es-widget.xml")
 	data = response.text
 	soup = BeautifulSoup(data, "xml")
@@ -15,10 +15,10 @@ def lambda_handler(event, context):
 	#Locate today's WOTD in database- must run query to find ID. Set result to wordID
 	dbQuery =  wotdDB.query(
 		TableName='wotd',
-        IndexName='language-word-index',
+        	IndexName='language-word-index',
 		ExpressionAttributeNames={"#L":"language"},
 		ExpressionAttributeValues={":lang":{"S":"spanish"},":word":{"S":soup.word.get_text()}},
-        KeyConditionExpression="#L = :lang AND word = :word"
+        	KeyConditionExpression="#L = :lang AND word = :word"
     )
 	wordID = dbQuery['Items'][0]['id']['N']
 
@@ -49,7 +49,7 @@ def lambda_handler(event, context):
 		TableName='wotd',
 		ExpressionAttributeNames={"#L":"language"},
 		ExpressionAttributeValues={":lang":{"S":"spanish"},":idVal":{"N":"0"},":isActiveVal":{"BOOL":True},":word":{"S":soup.word.get_text()}},
-        KeyConditionExpression="#L = :lang AND id > :idVal",
+        	KeyConditionExpression="#L = :lang AND id > :idVal",
 		FilterExpression="isActive = :isActiveVal AND word <> :word"
     )
 
