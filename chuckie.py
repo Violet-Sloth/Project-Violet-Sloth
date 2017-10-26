dd453eb6e29a4fd11638e710283eed6a43f044af
fromcj <fromcj@gmail.com>
2017-10-26 13:12:22 -0400

    Changed it
    Group fun

--- chuckie.py
+++ chuckie.py
@@ -13,7 +13,10 @@ def lambda_handler(event, context):
     print(wotd['Items'])
     item = wotd["Items"]
     print(item)
-    parsed = '<speak>The word of the day is <audio src="' + item[0]["word_sound"] + '"/> which means ' + item[0]["word_translation"] + '</speak>'
+    parsed = '<speak>The word of the day is <audio src="' + item[0]["word_sound"] + \
+    '"/> which means ' + item[0]["word_translation"] + \
+    '. Here is the word used in a sentence. <audio src="' + item[0]["sentence_sound"] + \
+    '"/> which means ' + item[0]["sentence_translation"] + '</speak>'
     response = {
         'version': '1.0',
         'response': {
