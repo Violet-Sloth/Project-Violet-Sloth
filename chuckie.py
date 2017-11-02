99868ed001a115d39cd299a1a4ff5def89e6cb48
fromcj <fromcj@gmail.com>
2017-11-02 13:39:44 -0400

    Update chuckie.py
    Cleaned up spoken response

--- chuckie.py
+++ chuckie.py
@@ -1,6 +1,5 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
-import os
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('wotd')
@@ -8,13 +7,13 @@ table = dynamodb.Table('wotd')
 
 def lambda_handler(event, context):
     wotd = table.query(
-        KeyConditionExpression=Key('language').eq(event["request"]["intent"]["slots"]["Language"]["value"].lower()) & Key('id').gt(0),
+        KeyConditionExpression=Key('language').eq(event['request']['intent']['slots']["Language"]["value"].lower()) & Key('id').gt(0),
         FilterExpression=Attr('isActive').eq(True)
     )
     print(wotd['Items'])
     item = wotd["Items"]
     print(item)
-    parsed = '<speak>The word of the day is <audio src="' + item[0]["word_sound"] + \
+    parsed = '<speak>The ' + event["request"]["intent"]["slots"]["Language"]["value"] + ' word of the day is <audio src="' + item[0]["word_sound"] + \
     '"/> which means ' + item[0]["word_translation"] + \
     '. Here is the word used in a sentence. <audio src="' + item[0]["sentence_sound"] + \
     '"/> which means ' + item[0]["sentence_translation"] + '</speak>'
